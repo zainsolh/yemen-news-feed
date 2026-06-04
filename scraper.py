b46a9f4777b039c8ac5277668dbbd1ab69d0e0c7
zainsolh <zain.solh2019@gmail.com>
2026-06-04 16:29:06 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,13 @@
 
+import json
+
+news = [
+    {
+        "title":"خبر تجريبي",
+        "link":"https://example.com",
+        "source":"تجربة"
+    }
+]
+
+with open("news.json","w",encoding="utf-8") as f:
+    json.dump(news,f,ensure_ascii=False)
