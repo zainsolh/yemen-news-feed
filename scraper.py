d26c3cf234ada0a5f137cc0487c6f7f127075ce5
zainsolh <zain.solh2019@gmail.com>
2026-06-05 17:39:21 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,28 +1,34 @@
-print("SCRAPER RUNNING")
 import requests
 from bs4 import BeautifulSoup
 import json
 
-news = []
-
 url = "https://www.almashhad.news/"
 
-r = requests.get(url, timeout=20)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+r = requests.get(url, headers=headers, timeout=20)
+
+print("Status:", r.status_code)
+print("Length:", len(r.text))
 
 soup = BeautifulSoup(r.text, "html.parser")
 
-for a in soup.find_all("a"):
+news = []
 
+for a in soup.find_all("a"):
     title = a.get_text(strip=True)
     link = a.get("href")
 
     if title and link and len(title) > 20:
-
         news.append({
             "title": title,
             "link": link,
             "source": "المشهد اليمني"
         })
 
+print("News found:", len(news))
+
 with open("news.json", "w", encoding="utf-8") as f:
-    json.dump(news[:30], f, ensure_ascii=False)
+    json.dump(news[:30], f, ensure_ascii=False, indent=2)
