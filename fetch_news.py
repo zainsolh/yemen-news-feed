a1b2c76c72d432d817c5f8e6708c962a9aed92f9
zainsolh <zain.solh2019@gmail.com>
2026-06-08 19:03:20 +0300

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -172,11 +172,13 @@ def publish_to_blogger(token, title, summary, image, source, link):
     html = ""
 
     if image:
-        html += f"""
-        <img src="{image}"
-             alt="{title}"
-             style="max-width:100%;height:auto;">
-        """
+    html += f"""
+    <img src="{image}"
+         alt="{title}"
+         style="max-width:100%;height:auto;">
+
+    <span><!--more--></span>
+    """
 
     html += f"""
 <p>{summary}</p>
