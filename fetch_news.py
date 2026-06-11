13d9984d55c0a0a1e9e482b1bba1f7d18cad5fed
zainsolh <zain.solh2019@gmail.com>
2026-06-11 18:04:19 +0300

    Fix formatting in fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -32,10 +32,10 @@ SOURCES = [
     {"name": "أنباء عدن", "url": "http://www.anbaaden.net/"},
     {"name": "الأحرار نت", "url": "http://www.al-ahrar.net/"},
     {"name": "الساحل", "url": "http://www.alsahil.net/"},
-    {"name": "ArabNN", "url": "http://www.arabnn.news/"}
+    {"name": "ArabNN", "url": "http://www.arabnn.news/"},
     {"name": "Arabkoora", "url": "https://www.kooora.com/?n=0&rss=1"},
 
-    { "name": "bin sport", "url":  "https://www.beinsports.com/ar-mena"},
+    { "name": "bin sport", "url":  "https://www.beinsports.com/ar-mena"}
     
 ]
 
